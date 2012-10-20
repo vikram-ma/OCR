7c86a28ce62f2f2e66978712c234492d7bfc4936
vikram-ma <vkrm206@gmail.com>
2012-10-21 00:19:05 +0530

    taking command line arguments instead of hard-coding

--- main.cpp
+++ main.cpp
@@ -4,17 +4,24 @@
 
 #ifndef _EiC
 #include <cv.h>
+#include <iostream>
 #include "OCR.h"
 #endif
 
 int main( int argc, char** argv )
 {
-    IplImage* imagev = cvLoadImage("./sampleUppercase.pbm", 0);
+    using std::cout;
+    using std::endl;
+    if(argc != 3)
+    {
+        cout <<"Usage <"<< argv[0]<<"> <path to directory containing samples> <test image>" << endl;
+    }
+    IplImage* imagev = cvLoadImage(argv[2], 0);
 	//////////////////
 	//My OCR
 	//////////////////
 	int* size = new int[1];
-	OCR ocr("./OCR/", 52, 3);
+	OCR ocr(argv[1], 52, 3);
 	ocr.classify(imagev, 1, size);
     return 0;
 }
